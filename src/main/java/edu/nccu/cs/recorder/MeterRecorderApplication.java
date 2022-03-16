37ee5ef2a74bb54587a977584d5d2c8cce756755
Gem Huang <gemhuang@iii.org.tw>
2022-03-16 08:03:37 +0800

    init project

---
+++ src/main/java/edu/nccu/cs/recorder/MeterRecorderApplication.java
@@ -0,0 +1,13 @@
+package edu.nccu.cs.recorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterRecorderApplication.class, args);
+	}
+
+}
